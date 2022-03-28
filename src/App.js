c9381789569fc99274f88e9200f56a70b9eb8b9c
William Mata <wmata0141@gmail.com>
2022-03-28 13:12:44 -0400

    Structure and title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <>
+        <section class="navigation">
+          <div class="nav-container">
+            <div class="brand">
+              <h3>Hacker News</h3>
+            </div>
+          </div>
+        </section>
+
+        <div className="App">
+          <div>Hola</div>
+        </div>
+      </>
     </div>
   );
 }
