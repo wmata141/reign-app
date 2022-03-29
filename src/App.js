5886b6ca693c275db9c7c2438d95883fa8fc7e88
William Mata <wmata0141@gmail.com>
2022-03-29 06:55:03 -0400

    Select Frameworks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, { useState } from 'react';
 import CorazonVacio from '../src/assets/images/corazonVacio.svg'
 import CorazonLleno from '../src/assets/images/corazonLleno.svg'
 import Timer from '../src/assets/images/timer.svg'
+import AngularImg from '../src/assets/images/image-138.png'
+import ReactImg from '../src/assets/images/image-140.png'
+import VueImg from '../src/assets/images/image-141.png'
 
 function App() {
+  const [framework, setFramework] = useState({ img: null, name: 'Select your News:' });
+
   return (
     <div className="Front-End-Test---Home-view">
 
@@ -11,7 +17,6 @@ function App() {
           HACKER NEWS
         </span>
       </div>
-
 
       <div className="RectangleContent">
         <div className="Rectangle">
@@ -27,14 +32,32 @@ function App() {
         </div>
       </div>
 
-      <div>
-        <div className="Rectangle-26-Copy-23">
+      <div className="Rectangle-26-Copy-23 dropdown">
+        <button className="dropbtn">
           <span className="Text">
-            Select your news
+            <img src={framework.img}
+              className="Image-138" />
+            {framework.name} 
           </span>
-          <div className="Mask">
+        </button>
 
-          </div>
+
+        <div className="dropdown-content">
+          <span onClick={() => setFramework({ img: AngularImg, name: 'Angular' })} className="Text">
+            <img src={AngularImg}
+              className="Image-138" />
+            Angular
+          </span>
+          <span onClick={() => setFramework({ img: ReactImg, name: 'React' })} className="Text">
+            <img src={ReactImg}
+              className="Image-138" />
+            React
+          </span>
+          <span onClick={() => setFramework({ img: VueImg, name: 'Vue' })} className="Text">
+            <img src={VueImg}
+              className="Image-138" />
+            Vue
+          </span>
         </div>
       </div>
 
@@ -62,8 +85,6 @@ function App() {
           })
         }
       </div>
-
-
 
     </div>
   );
