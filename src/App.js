01ff983e7a0f3201d37f91bb534f20d03a9a781a
William Mata <wmata0141@gmail.com>
2022-03-30 14:27:10 -0400

    Paginator Upgrade

--- src/App.js
+++ src/App.js
@@ -13,56 +13,24 @@ function App() {
   const [faves, setFaves] = useState([]);
   const [loading, setLoading] = useState(true);
   const [listHandle, setListHandle] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(6);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [nbPages, setNbPages] = useState([])
 
   useEffect(() => {
-    const fetchPost = async () => {
-      const data = await fetch(`https://hn.algolia.com/api/v1/search_by_date?query=${framework.name}&page=0`)
-      const { hits } = await data.json()
-
-      const arrayAll = []
-      hits.forEach(element => {
-        const resultado = faves.find(item => item.objectID === element.objectID);
-
-        if (resultado) {
-          arrayAll.push(resultado)
-        } else {
-          arrayAll.push(element)
-        }
-      });
-
-      setPosts(arrayAll)
-      setLoading(false)
-    }
-
     fetchPost()
-  }, [framework, faves])
+  }, [framework, faves, currentPage])
 
   useEffect(() => {
     const frameworkJson = JSON.parse(localStorage.getItem('framework'))
-    if (frameworkJson) setFramework(frameworkJson)    
+    if (frameworkJson) setFramework(frameworkJson)
 
     const favesJson = JSON.parse(localStorage.getItem('faves'))
     if (favesJson) setFaves(favesJson)
-    
+
   }, [])
 
-
-
   const now = moment();
-
-  const indexOfLastPost = currentPage * postsPerPage
-  const indexOfFirstPost = indexOfLastPost - postsPerPage
-  let currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
-
-  if (!listHandle) {
-    currentPosts = faves.slice(indexOfFirstPost, indexOfLastPost)
-  }
-
-  console.log("currentPosts ==>", currentPosts);
-
-  const pageNumbers = []
 
   const frameworkList = [
     { img: AngularImg, name: 'angular' },
@@ -70,13 +38,51 @@ function App() {
     { img: VueImg, name: 'vue' }
   ]
 
-  for (let i = 1; i <= Math.ceil(posts.length / postsPerPage); i++) {
-    pageNumbers.push(i)
+  let currentPosts = posts
+  if (!listHandle) {
+    currentPosts = faves
+  }
+
+  const fetchPost = async () => {
+    const data = await fetch(`https://hn.algolia.com/api/v1/search_by_date?query=${framework.name}&page=${currentPage}`)
+    const { hits, nbPages } = await data.json()
+
+    const pageNumbers = []
+    let initPage = 0
+    if (currentPage - 3 <= 0) {
+      initPage = 0 
+    } else {
+      initPage = currentPage - 3
+    }
+
+    for (let i = initPage; i <= nbPages - 1; i++) {
+      if (i <= currentPage + 3 || i <= currentPage - 3) {
+        console.log(i);
+        pageNumbers.push(i)
+      }
+    }
+    setNbPages(pageNumbers)
+
+    const arrayAll = []
+    hits.forEach(element => {
+      const resultado = faves.find(item => item.objectID === element.objectID);
+
+      if (resultado) {
+        arrayAll.push(resultado)
+      } else {
+        arrayAll.push(element)
+      }
+    });
+
+    setPosts(arrayAll)
+    setLoading(false)
+    setTotalPages(nbPages - 1)
   }
 
   const handleFaves = (item) => {
     const arrayAuxPosts = posts
     const arrayAuxFaves = faves
+
     if (item.faves) {
       arrayAuxPosts.forEach(element => {
         if (element.objectID === item.objectID) {
@@ -97,10 +103,6 @@ function App() {
       localStorage.setItem('faves', JSON.stringify(result))
       setFaves(result)
     }
-
-    console.log("item ==>", item);
-
-
   }
 
   const handleFramework = (item) => {
@@ -134,90 +136,96 @@ function App() {
         </div>
       </div>
 
-      {
-        loading ? (
-          <div style={{ textAlign: 'center' }}>
-            <div className="sp sp-3balls"></div>
-          </div>
-        ) : (
-          <>
-            <div className="Rectangle-26-Copy-23 dropdown">
-              <button className="dropbtn">
-                <span className="Text">
-                  <img src={framework.img} alt="" className="Image-138" />
-                  {framework.name[0].toUpperCase() + framework.name.slice(1)}
-                </span>
-              </button>
-              <div className="dropdown-content">
-                {
-                  frameworkList.map((item, i) => {
-                    return (
-                      <span key={i} onClick={() => handleFramework(item)} className="Text">
-                        <img src={item.img} alt="Angular" className="Image-138" />
-                        {item.name[0].toUpperCase() + item.name.slice(1)}
-                      </span>
-                    )
-                  })
-                }
-              </div>
+      <div>
+        {
+          loading ? (
+            <div style={{ textAlign: 'center' }}>
+              <div className="sp sp-3balls"></div>
             </div>
-
-            <div className="Reactangle-Card-Content">
-              {
-                currentPosts.map((post, i) => {
-                  const duration = moment.duration(now.diff(post.created_at));
-                  let agoByAuthor = `${duration._data.hours} hours ago by ${post.author}`
-
-                  if (duration._data.hours < 1) {
-                    agoByAuthor = `${duration._data.minutes} minutes ago by  ${post.author}`
+          ) : (
+            <>
+              <div className="Rectangle-26-Copy-23 dropdown">
+                <button className="dropbtn">
+                  <span className="Text">
+                    <img src={framework.img} alt="" className="Image-138" />
+                    {framework.name[0].toUpperCase() + framework.name.slice(1)}
+                  </span>
+                </button>
+                <div className="dropdown-content">
+                  {
+                    frameworkList.map((item, i) => {
+                      return (
+                        <span key={i} onClick={() => handleFramework(item)} className="Text">
+                          <img src={item.img} alt="Angular" className="Image-138" />
+                          {item.name[0].toUpperCase() + item.name.slice(1)}
+                        </span>
+                      )
+                    })
                   }
+                </div>
+              </div>
 
-                  return (
-                    <div className="Rectangle-Card" key={i}>
-                      <div style={{ display: 'grid' }} onClick={() => clickOpen(post.story_url)}>
-                        <span className="-hours-ago-by-autho">
-                          <img src={Timer} alt="timer" className="iconmonstr-time-2" />
-                          {agoByAuthor}
-                        </span>
+              <div className="Reactangle-Card-Content">
+                {
+                  currentPosts.map((post, i) => {
+                    const duration = moment.duration(now.diff(post.created_at));
+                    let agoByAuthor = `${duration._data.hours} hours ago by ${post.author}`
 
-                        <span className="Yes-React-is-taking">
-                          {post.story_title}
-                        </span>
+                    if (duration._data.hours < 1) {
+                      agoByAuthor = `${duration._data.minutes} minutes ago by  ${post.author}`
+                    }
+
+                    return (
+                      <div className="Rectangle-Card" key={i}>
+                        <div style={{ display: 'grid' }} onClick={() => clickOpen(post.story_url)}>
+                          <span className="-hours-ago-by-autho">
+                            <img src={Timer} alt="timer" className="iconmonstr-time-2" />
+                            {agoByAuthor}
+                          </span>
+
+                          <span className="Yes-React-is-taking">
+                            {post.story_title}
+                          </span>
+                        </div>
+
+                        <div className="Rectangle-Right" onClick={() => handleFaves(post)}>
+                          {
+                            post.faves ? (
+                              <img src={CorazonLleno} alt="heart" className="iconmonstr-favorite-3" />
+                            ) : (
+                              <img src={CorazonVacio} alt="heart" className="iconmonstr-favorite-3" />
+                            )
+                          }
+
+                        </div>
                       </div>
-
-                      <div className="Rectangle-Right" onClick={() => handleFaves(post)}>
-                        {
-                          post.faves ? (
-                            <img src={CorazonLleno} alt="heart" className="iconmonstr-favorite-3" />
-                          ) : (
-                            <img src={CorazonVacio} alt="heart" className="iconmonstr-favorite-3" />
-                          )
-                        }
-
-                      </div>
-                    </div>
-                  )
-                })}
-            </div>
-          </>
-        )
-      }
+                    )
+                  })}
+              </div>
+            </>
+          )
+        }
+      </div>
 
       <div>
         <nav className="footer">
           <ul className='pagination'>
-            <a onClick={() => setCurrentPage(1)} href="!#" className="Rectangle-3-Copy-33">
+          <a onClick={() => setCurrentPage(0)} href="!#" className="Rectangle-3-Copy-33">
+              {'<<'}
+            </a>
+            <a onClick={() => setCurrentPage(currentPage - 1)} href="!#" className="Rectangle-3-Copy-33">
               {'<'}
             </a>
-            {pageNumbers.map(number => (
-              // <li key={number} className="page-item">
+            {nbPages.map(number => (
               <a key={number} onClick={() => setCurrentPage(number)} href="!#" className={number === currentPage ? "Rectangle-3-Copy-33-active" : "Rectangle-3-Copy-33"}>
                 {number}
               </a>
-              // </li>
             ))}
-            <a onClick={() => setCurrentPage(pageNumbers.length)} href="!#" className="Rectangle-3-Copy-33">
+            <a onClick={() => setCurrentPage(currentPage + 1)} href="!#" className="Rectangle-3-Copy-33">
               {'>'}
+            </a>
+            <a onClick={() => setCurrentPage(totalPages)} href="!#" className="Rectangle-3-Copy-33">
+              {'>>'}
             </a>
           </ul>
         </nav>
